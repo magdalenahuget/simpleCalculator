90b4f1458649626a83c02d06b065d6f37776ce24
magdalenahuget <magdalena.huget@gmail.com>
2022-11-15 23:28:24 +0100

    Add resolution

---
+++ data.js
@@ -0,0 +1,35 @@
+// My lovely calculator after 6 years
+
+function addition(a, b){
+    let result = a + b;
+    return result;
+}
+function subtraction(c, d){
+    let result = c - d;
+    return result;
+}
+function multiplication(a, b){
+    let result = a * b
+    return result;
+}
+function division(a, b){
+    let result = a / b;
+    return result;
+}
+function modulo(a, b){
+    let result = a % b;
+    return result;
+}
+
+// calculation functions
+
+let sum = addition(10, 5);
+let difference = subtraction(100, 90);
+let product = multiplication(5, 5);
+let quotient = division(10, 3);
+let moduloResult = modulo(13, 5);
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(moduloResult);
